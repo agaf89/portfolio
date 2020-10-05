38718d5b7c2f0b47edc050b6794b38680f344432
Stanislav <agafon_stas89@mail.ru>
2020-10-05 23:02:07 +0300

    'contacts'

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,15 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closes = document.querySelector('.menu__close');
 const overlay = document.querySelector('.menu__overlay');
+const percent = document.querySelectorAll('.list__item-percent');
+const divActive = document.querySelectorAll('.list__item-active');
 
+
+function percentElement(){
+    percent.forEach( (item,i) =>{
+        divActive[i].style.width = item.textContent;
+    });
+}
 function toggleClass(listener){
     listener.addEventListener('click',()=>{
         menu.classList.toggle('active');
@@ -11,3 +19,5 @@ function toggleClass(listener){
 toggleClass(hamburger);
 toggleClass(closes);
 toggleClass(overlay);
+percentElement();
+
